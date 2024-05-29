8d926f5967f26b9ecc6188d6096a668e83ab3745
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-05-29 10:37:07 +0300

    Weather page

---
+++ resources/js/Pages/weather.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+defineProps({
+    weatherData: {
+        type: Object,
+    },
+});
+</script>
+
+<template>
+    <Head title="Weather"></Head>
+    <div>
+        <img
+            style="background-color: gray"
+            :src="`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`"
+            alt="Weather icon"
+        />
+        <span>{{ weatherData.name }}</span>
+        <span>{{ weatherData.main.temp }}</span>
+        <span>{{ weatherData.weather[0].description }}</span>
+    </div>
+    <div>
+        <h1>Weather</h1>
+        <ul>
+            <li>Feels like {{ weatherData.main.feels_like }}</li>
+            <li>Humidity {{ weatherData.main.humidity }}</li>
+            <li>Pressure {{ weatherData.main.pressure }}</li>
+            <li>Temp min {{ weatherData.main.temp_min }}</li>
+            <li>Temp max {{ weatherData.main.temp_max }}</li>
+        </ul>
+        <h1>Wind</h1>
+        <ul>
+            <li>Wind speed {{ weatherData.wind.speed }}</li>
+            <li>Wind degrees {{ weatherData.wind.deg }}</li>
+        </ul>
+    </div>
+</template>
