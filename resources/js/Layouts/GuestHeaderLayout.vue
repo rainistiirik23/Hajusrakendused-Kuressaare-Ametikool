dd9c60285d6ab31a23e2710b577c21281b6fe4b8
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-05 17:02:23 +0300

    Header layout for unauthenticated user

---
+++ resources/js/Layouts/GuestHeaderLayout.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from "vue";
+import ApplicationLogo from "@/Components/ApplicationLogo.vue";
+import { Link } from "@inertiajs/vue3";
+
+const showingNavigationDropdown = ref(false);
+</script>
+
+<template>
+    <div>
+        <div>
+            <nav class="bg-white border-b border-gray-100">
+                <!-- Primary Navigation Menu -->
+                <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div class="flex justify-between h-16">
+                        <div class="flex">
+                            <!-- Logo -->
+                            <div class="shrink-0 flex items-center">
+                                <a :href="route('dashboard')">
+                                    <ApplicationLogo
+                                        class="block h-9 w-auto fill-current text-gray-800"
+                                    />
+                                </a>
+                            </div>
+                        </div>
+
+                        <div class="sm:flex sm:items-center sm:ms-6">
+                            <Link :href="route('login')">Log in</Link>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+
+            <!-- Page Heading -->
+            <header class="bg-white shadow" v-if="$slots.header">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    <slot name="header" />
+                </div>
+            </header>
+        </div>
+    </div>
+</template>
