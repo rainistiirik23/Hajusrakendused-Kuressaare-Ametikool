a7043c2b4a39bbcc98ba75649a0484f0c342048e
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-15 12:24:18 +0300

    Added products link

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -54,6 +54,12 @@ function handleImageError() {
                     </div>
                     <nav v-if="canLogin" class="-mx-3 flex flex-1 justify-end">
                         <Link
+                            :href="route('products-view')"
+                            class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
+                        >
+                            Products
+                        </Link>
+                        <Link
                             v-if="$page.props.auth.user"
                             :href="route('dashboard')"
                             class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
