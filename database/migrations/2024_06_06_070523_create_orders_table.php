c367cba7c80f55b86a38fa254dd0bf9f36d0250c
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-15 12:22:16 +0300

    Order,order items and products migrations

---
+++ database/migrations/2024_06_06_070523_create_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->string('phone');
+            $table->bigInteger('order_sum');
+            $table->timestamp('added_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
