e54e90ffbc158db9d3ff219ebead8e21d93d73ca
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-15 17:56:40 +0300

    Api routes setup

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\MovieController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/movies/NewMovie', [MovieController::class, 'store'])->name('movie-store');
+Route::get('/movies/FetchMovies/{limit}', [MovieController::class, 'show'])->name('movie-store');
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
