4ceedd85aae6588e8551e06aa486bac208e597b7
rainistiirik23 <rainis.tiirik9@gmail.com>
2024-06-16 18:52:37 +0300

    Removed limit paramater

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/movies/NewMovie', [MovieController::class, 'store'])->name('movie-store');
-Route::get('/movies/FetchMovies/{limit}', [MovieController::class, 'show'])->name('movie-store');
+Route::get('/movies/FetchMovies', [MovieController::class, 'show'])->name('movie-store');
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
